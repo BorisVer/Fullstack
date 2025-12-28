5f5499552eaccd02bdbff52c88c32d04f9d581e3
BorisVer <borisversonnen@gmail.com>
2025-12-28 12:31:20 +0200

    mm

--- part3/puhelinluettelo/backend/index.js
+++ part3/puhelinluettelo/backend/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
 app.use(morgan("tiny"));
 
 let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+  { id: 4, name: "Herra Backend", number: "666-666-666" },
 ];
 
 const date = new Date();
@@ -52,6 +56,10 @@ app.post("/api/persons", (request, response) => {
   const person = { id, name, number };
   persons.push(person);
   response.json(person);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 const PORT = process.env.port || 3001;
