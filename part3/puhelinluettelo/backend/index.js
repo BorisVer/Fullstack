2bea21c41c82d8aab16ac7443d9d416ea2bef4a7
BorisVer <borisversonnen@gmail.com>
2026-09-02 18:20:23 +0300

    Added 3.8

--- part3/puhelinluettelo/backend/index.js
+++ part3/puhelinluettelo/backend/index.js
@@ -8,7 +8,17 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (request) => {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  }
+  return ''
+})
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 app.use(express.static('dist'))
 
 // Routes
